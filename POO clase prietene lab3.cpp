d319bf9ce60ab86fc3b14ad0c4aeea35a038f7c2
SabauClaudiu23 <claudiusabau23@yahoo.com>
2025-04-01 14:03:42 +0300

    calse_prietene

---
+++ POO clase prietene lab3.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Persoana {
+protected:
+    string nume;
+    int varsta;
+
+public:
+    Persoana(string nume, int varsta)
+    {
+        this->nume = nume;
+        this->varsta = varsta;
+
+    }
+    void afiseazaInformatii()
+    {
+        cout << "Nume: " << nume << "Varsta: " << varsta << endl;
+    }
+};
+// STUDENT -------------------------------------------------------------------------
+class Student : public Persoana {
+private:
+    float medie;
+    friend class Admin;
+public:
+    Student(string nume, int varsta, float medie) : Persoana(nume, varsta)
+    {
+        this->medie = medie;
+    }
+    void afiseazaInformatii()
+    {
+        cout << "Nume: " << nume << "Varsta: " << varsta << "Medie: " << medie << endl;
+    }
+};
+
+//Suprascierea operatoruluui
+bool operator>(const Student celalalt_obiect) const {
+    return this->medie> celalalt_obiect
+}
+
+class Admin {
+public:
+    void modificaMedie(Student& s, float nouaMedie)
+    {
+        cout << "Modific media pentru: " << s.nume << "de la" << s.medie << "la" << nouaMedie << endl;
+        s.medie = nouaMedie;
+    }
+    void afiseazaDetaliiStudent(const Student& s) {
+        cout << "Admin -> Student" << s.nume << ",Varsta: " << s.varsta << ",Medie: " << s.medie << endl;
+
+    }
+};
+int main()
+{
+    Student s1("Gabi ", 18, 10);
+    Student s2("Claudiu ", 24, 4);
+    s1.afiseazaInformatii();
+    s2.afiseazaInformatii();
+    Admin admin;
+    admin.modificaMedie(s2, 2);
+    admin.afiseazaDetaliiStudent(s2);
+
+
+
+
+}
